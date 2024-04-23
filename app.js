6e0fc6a3af1c81cf84389ea17232acc561a926ae
anugondi <agondi@unc.edu>
2024-04-23 17:32:50 -0400

    Progress on integrating spotify API

---
+++ app.js
@@ -0,0 +1,79 @@
+const APIController = (function() {
+    const clientId = 'cb9546ca89e545669cb57ec1c0f23a95';
+    const clientSecret = '1f3b695233124e78a96eccbf73c1a3ae';
+
+    const _getToken = async () => {
+        const result = await fetch('https://accounts.spotify.com/api/token', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization' : 'Basic ' + btoa(clientId + ':' + clientSecret)
+            }, 
+            body: 'grant_type=client_credentials'
+        });
+        const data = await result.json();
+        return data.access_token;
+    }
+
+    const _getGenres = async (token) => {
+        const result = await fetch(`https://api.spotify.com/v1/browse/categories?locale=sv_US`, {
+            method: 'GET',
+            headers: {'Authorization' : 'Bearer ' + token}
+        });
+        const data = await result.json();
+        return data.categories.items;
+    }
+
+    const _getPlaylistByGenre = async (token, genreId) => {
+        const limit =  10;
+        const result = await fetch (`https://api.spotify.com/v1/browse/categories/${genreId}/playlists?limit=${limit}`, {
+            method: 'GET',
+            heaaders: {'Authorization' : 'Bearer ' + token}
+        });
+        const data = await result.json();
+        return data.playlists.items;
+    }
+    
+    const _getTracks = async (token, tracksEndPoint) => {
+        const limit =  10;
+        const result = await fetch (`${tracksEndPoint}?limit=${limit}`, {
+            method: 'GET',
+            heaaders: {'Authorization' : 'Bearer ' + token}
+        });
+        const data = await result.json();
+        return data.items;
+    }
+
+    const _getTrack = async (token, tracksEndPoint) => {
+        const limit =  10;
+        const result = await fetch (`${trackEndPoint}`, {
+            method: 'GET',
+            heaaders: {'Authorization' : 'Bearer ' + token}
+        });
+        const data = await result.json();
+        return data;
+    }
+
+    return{
+        getToken() {
+            return _getToken();
+        }, 
+        getGenres(token) {
+            return _getGenres(token);
+        }, 
+        getPlaylistByGenre(token, genreId) {
+            return _getPlaylistByGenre(token, genreId);
+        },
+        getTracks(token, tracksEndPoint) {
+            return _getTracks(token, tracksEndPoint);
+        }, 
+        getTrack(token, trackEndPoint) {
+            return _getTrack(token, trackEndPoint);
+        }
+    }
+})();
+
+// UI Component
+const UIController = (function() {
+    
+})();
